f2d619089150eb2ebe1de07e69ca8c9801374c9c
Meshack <meshack.odunowe@decagon.dev>
2025-03-20 21:41:37 +0100

    added buttons for sccessibility

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,26 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex gap-4 h-screen items-center justify-center">
       <button className="  text-white px-2 py-4 rounded-lg bg-blue-600">
 
      <Link href='/login'>Go to Login Page </Link>
+      </button>
+      <button className="  text-white px-2 py-4 rounded-lg bg-blue-600">
+
+     <Link href='/reset-password'>Link to Password Reset page </Link>
+      </button>
+      <button className="  text-white px-2 py-4 rounded-lg bg-blue-600">
+
+     <Link href='/control-tower'>Link to Control Tower  </Link>
+      </button>
+      <button className="  text-white px-2 py-4 rounded-lg bg-blue-600">
+
+     <Link href='/forgot-password'>Link to Forgot Password  </Link>
+      </button>
+      <button className="  text-white px-2 py-4 rounded-lg bg-blue-600">
+
+     <Link href='/cash-request'>Link to Cash Request  </Link>
       </button>
     </div>
   );
